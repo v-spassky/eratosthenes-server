a374ad89b5b4500fc561c3fc3aa0e4d95e99e6b4
Vasiliy Spassky <spassky99@gmail.com>
2024-03-28 22:35:36 +0200

    Calculate guess distance correctly

--- src/map_locations.rs
+++ src/map_locations.rs
@@ -2,6 +2,8 @@ use crate::models::LatLng;
 use rand::Rng;
 
 static LOCATIONS: [(f64, f64); 840] = include!("street_view_locations.txt");
+const EARTH_RADIUS: f64 = 6371e3;
+const MAX_SCORE: u64 = 2000;
 
 pub fn get_random_position() -> LatLng {
     let mut rng = rand::thread_rng();
@@ -13,9 +15,14 @@ pub fn get_random_position() -> LatLng {
 }
 
 pub fn get_guess_score(guess: LatLng, target: LatLng) -> u64 {
-    let lat_diff = (guess.lat - target.lat).abs();
-    let lng_diff = (guess.lng - target.lng).abs();
-    let distance = (lat_diff.powi(2) + lng_diff.powi(2)).sqrt();
-    let score = (1.0 / distance) * 1000.0;
-    (score as u64).min(2000)
+    let phi_1 = guess.lat * std::f64::consts::PI / 180.0;
+    let phi_2 = target.lat * std::f64::consts::PI / 180.0;
+    let delta_phi = (target.lat - guess.lat) * std::f64::consts::PI / 180.0;
+    let delta_lambda = (target.lng - guess.lng) * std::f64::consts::PI / 180.0;
+    let a = (delta_phi / 2.0).sin().powi(2)
+        + phi_1.cos() * phi_2.cos() * (delta_lambda / 2.0).sin().powi(2);
+    let c = 2.0 * (a.sqrt().atan2((1.0 - a).sqrt()));
+    let distance = EARTH_RADIUS * c;
+    let score = (1.0 / distance) * 1e8;
+    (score as u64).min(MAX_SCORE)
 }
