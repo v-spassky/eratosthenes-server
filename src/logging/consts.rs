1f5a8efe2b0695e8a2349e56f031d524f33943a1
Vasiliy Spassky <spassky99@gmail.com>
2024-12-03 16:52:46 +0200

    feat: move Quickwit tracing layer to a separate crate

--- src/logging/consts.rs
+++ src/logging/consts.rs
@@ -2,5 +2,3 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 pub const DEFAULT_CLIENT_IP: SocketAddr =
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-pub const DEFAULT_LOGGING_BUFFER_SIZE: usize = 500;
-pub const QUICKWIT_LOGGING_CHANNEL_CAPACITY: usize = 512;
--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,23 +1,24 @@
 use crate::cli::Args;
-use crate::logging::quickwit::QuickwitLoggingLayerBuilder;
+use tracing_quickwit::QuickwitLoggingLayerBuilder;
 use tracing_subscriber::fmt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 
 pub mod consts;
-pub mod quickwit;
 
 pub fn init(args: &Args) {
-    let quickwit_logging_layer = QuickwitLoggingLayerBuilder::new(args.quickwit_url.clone())
-        .marker_field("task")
-        .map_marker_to_index("http_request", "http_requests")
-        .map_marker_to_index("client_sent_ws_message", "client_sent_ws_messages")
-        .map_marker_to_index("sockets_count", "sockets_counts")
-        .with_batch_size(100)
-        .build();
-    let env_filter = EnvFilter::default()
-        .add_directive("eratosthenes_server=info".parse().unwrap());
+    let (quickwit_logging_layer, quickwit_background_client_task) =
+        QuickwitLoggingLayerBuilder::new(args.quickwit_url.clone())
+            .marker_field("task")
+            .map_marker_to_index("http_request", "http_requests")
+            .map_marker_to_index("client_sent_ws_message", "client_sent_ws_messages")
+            .map_marker_to_index("sockets_count", "sockets_counts")
+            .with_batch_size(100_usize)
+            .build();
+    tokio::spawn(quickwit_background_client_task);
+    let env_filter =
+        EnvFilter::default().add_directive("eratosthenes_server=info".parse().unwrap());
     tracing_subscriber::registry()
         .with(quickwit_logging_layer)
         .with(env_filter)
